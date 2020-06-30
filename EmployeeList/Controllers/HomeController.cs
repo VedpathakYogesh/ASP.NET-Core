46e9ac70ed2a89e55795af12bf2de01f7968a7fe
YogeshVedpathak3777 <yogesh.vedpathak@yash.com>
2020-07-01 00:30:39 +0530

    Entity Framework Database First In ASP.NET Core

---
+++ EmployeeList/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EmployeeList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly EmployeeContext _dbContext;
+
+        public HomeController(EmployeeContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var _emplst = _dbContext.tblEmployees.
+                            Join(_dbContext.tblSkills, e => e.SkillID, s => s.SkillID,
+                            (e, s) => new EmployeeViewModel
+                            {
+                                EmployeeID = e.EmployeeID,
+                                EmployeeName = e.EmployeeName,
+                                PhoneNumber = e.PhoneNumber,
+                                Skill = s.Title,
+                                YearsExperience = e.YearsExperience
+                            }).ToList();
+            IList<EmployeeViewModel> emplst = _emplst;
+            return View(emplst);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EmployeeList/Models/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeList.Models;
+
+namespace EmployeeList.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<tblSkill> tblSkills { get; set; }
+        public DbSet<tblEmployee> tblEmployees { get; set; }
+        public DbSet<EmployeeList.Models.EmployeeViewModel> EmployeeViewModel { get; set; }
+    }
+}
